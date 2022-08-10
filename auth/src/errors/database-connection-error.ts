691d201fe6a2879d3fd8d03e86f68a884ac4b306
Ori Baram <obrm770@gmail.com>
2022-08-10 12:34:47 +0300

    first commit

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,20 @@
+import { CustomError } from './custom-error'
+
+export class DatabaseConnectionError extends CustomError {
+  reason = 'Error connecting to database'
+  statusCode = 500
+
+  constructor() {
+    super('Error connecting to database')
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ]
+  }
+}
