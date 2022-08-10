16cc1bb36c1b0906e43a59bd5a5bb465ef78c848
Ori Baram <obrm770@gmail.com>
2022-08-10 15:29:24 +0300

    auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -23,6 +24,16 @@ app.all('*', async () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to MongoDB')
+  } catch (err) {
+    console.error(err)
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000')
+  })
+}
+
+start()
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { BadRequestError } from './../errors/bad-request-error'
 
 const router = express.Router()
 
@@ -14,7 +15,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
@@ -23,10 +24,16 @@ router.post(
 
     const { email, password } = req.body
 
-    console.log('Creating a user...')
-    throw new DatabaseConnectionError()
+    const existingUser = await User.findOne({ email })
 
-    res.send({})
+    if (existingUser) {
+      throw new BadRequestError('Email is in use')
+    }
+
+    const user = User.build({ email, password })
+    await user.save()
+
+    res.status(201).send(user)
   }
 )
 
