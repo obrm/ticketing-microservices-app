9a06aad6e4cb917540a8cee244b46a0648bee572
Ori Baram <obrm770@gmail.com>
2022-08-10 16:45:22 +0300

    auth

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from './../errors/bad-request-error'
@@ -32,6 +34,18 @@ router.post(
 
     const user = User.build({ email, password })
     await user.save()
+
+    const userJwt = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    )
+
+    req.session = {
+      jwt: userJwt,
+    }
 
     res.status(201).send(user)
   }
