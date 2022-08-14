206f88a5bcdb40bc735e3266e3cf7d64f74a23d3
Ori Baram <obrm770@gmail.com>
2022-08-14 15:55:06 +0300

    signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -20,3 +20,46 @@ it('returns a 400 with an invalid email', async () => {
     })
     .expect(400)
 })
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '1',
+    })
+    .expect(400)
+})
+
+it('returns a 400 without an email and password', async () => {
+  return request(app).post('/api/users/signup').send({}).expect(400)
+})
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+  expect(response.get('Set-Cookie')).toBeDefined()
+})
