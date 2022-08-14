2a750a2c0df0b22da93fed9cfe8bf7642107e15d
Ori Baram <obrm770@gmail.com>
2022-08-14 17:25:57 +0300

    auth tests complete

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,65 +1,78 @@
-import request from 'supertest'
-import { app } from '../../app'
+import request from "supertest";
+import { app } from "../../app";
 
-it('returns a 201 on successful signup', async () => {
-  return request(app)
-    .post('/api/users/signup')
+it("returns a 201 on successful signup", async () => {
+  return await request(app)
+    .post("/api/users/signup")
     .send({
-      email: 'test@test.com',
-      password: 'password',
+      email: "test@test.com",
+      password: "password",
     })
-    .expect(201)
-})
+    .expect(201);
+});
 
-it('returns a 400 with an invalid email', async () => {
-  return request(app)
-    .post('/api/users/signup')
+it("returns a 400 with an invalid email", async () => {
+  return await request(app)
+    .post("/api/users/signup")
     .send({
-      email: 'testtest.com',
-      password: 'password',
+      email: "alskdflaskjfd",
+      password: "password",
     })
-    .expect(400)
-})
+    .expect(400);
+});
 
-it('returns a 400 with an invalid password', async () => {
-  return request(app)
-    .post('/api/users/signup')
+it("returns a 400 with an invalid password", async () => {
+  return await request(app)
+    .post("/api/users/signup")
     .send({
-      email: 'test@test.com',
-      password: '1',
+      email: "alskdflaskjfd",
+      password: "p",
     })
-    .expect(400)
-})
+    .expect(400);
+});
 
-it('returns a 400 without an email and password', async () => {
-  return request(app).post('/api/users/signup').send({}).expect(400)
-})
-
-it('disallows duplicate emails', async () => {
+it("returns a 400 with missing email and password", async () => {
   await request(app)
-    .post('/api/users/signup')
+    .post("/api/users/signup")
     .send({
-      email: 'test@test.com',
-      password: 'password',
+      email: "test@test.com",
     })
-    .expect(201)
+    .expect(400);
 
   await request(app)
-    .post('/api/users/signup')
+    .post("/api/users/signup")
     .send({
-      email: 'test@test.com',
-      password: 'password',
+      password: "alskjdf",
     })
-    .expect(400)
-})
+    .expect(400);
+});
 
-it('sets a cookie after successful signup', async () => {
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("sets a cookie after successful signup", async () => {
   const response = await request(app)
-    .post('/api/users/signup')
+    .post("/api/users/signup")
     .send({
-      email: 'test@test.com',
-      password: 'password',
+      email: "test@test.com",
+      password: "password",
     })
-    .expect(201)
-  expect(response.get('Set-Cookie')).toBeDefined()
-})
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,28 +1,52 @@
-import { MongoMemoryServer } from 'mongodb-memory-server'
-import mongoose from 'mongoose'
-import { app } from '../app'
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
 
-let mongo: any
+declare global {
+  function signin(): Promise<string[]>;
+}
+
+let mongo: any;
 
 beforeAll(async () => {
-  process.env.JWT_KEY = 'fjdkfd'
-  mongo = await MongoMemoryServer.create()
-  const mongoUri = mongo.getUri()
+  process.env.JWT_KEY = "asdfasdf";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-  await mongoose.connect(mongoUri, {})
-})
+  const mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
 
 beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections()
+  const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
-    await collection.deleteMany({})
+    await collection.deleteMany({});
   }
-})
+});
 
 afterAll(async () => {
   if (mongo) {
-    await mongo.stop()
+    await mongo.stop();
   }
-  await mongoose.connection.close()
-})
+  await mongoose.connection.close();
+});
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
