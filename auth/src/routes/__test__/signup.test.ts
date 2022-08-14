91f8455928b1bbede595b0397ec553bb90d95ceb
Ori Baram <obrm770@gmail.com>
2022-08-14 15:41:30 +0300

    tests

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+})
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'password',
+    })
+    .expect(400)
+})
