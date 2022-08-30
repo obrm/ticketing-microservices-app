9c743d6d1f822eb0849a1e4f2b08eff017731522
Ori Baram <obrm770@gmail.com>
2022-08-30 13:53:05 +0300

    NATS

---
+++ nats-test/src/events/base-listener.ts
@@ -0,0 +1,47 @@
+import { Message, Stan } from 'node-nats-streaming'
+import { Subjects } from './subjects'
+import { Event } from './event'
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T['subject']
+  abstract onMessage(data: T['data'], msg: Message): void
+  abstract queueGroupName: string
+  private client: Stan
+  protected ackWait = 5 * 1000
+
+  constructor(client: Stan) {
+    this.client = client
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName)
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    )
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`)
+
+      const parseData = this.parseMessage(msg)
+      this.onMessage(parseData, msg)
+    })
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData()
+
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'))
+  }
+}
---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,15 @@
+import { Message } from 'node-nats-streaming'
+import { Listener } from './base-listener'
+import { Subjects } from './subjects'
+import { TicketCreatedEvent } from './ticket-created-event'
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated
+  queueGroupName = 'payments-service'
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log('Event data', data)
+
+    msg.ack()
+  }
+}
